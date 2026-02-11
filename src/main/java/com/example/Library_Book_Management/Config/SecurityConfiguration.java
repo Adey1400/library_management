89bf19df2e956c3ee565cf472189424613bcdd70
Subhankito Roy Choudhury <subhankitoroychoudhuryofficial@gmail.com>
2026-02-11 10:55:11 +0530

    configure package.json

--- src/main/java/com/example/Library_Book_Management/Config/SecurityConfiguration.java
+++ src/main/java/com/example/Library_Book_Management/Config/SecurityConfiguration.java
@@ -10,8 +10,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+
 
 import java.util.List;
 
